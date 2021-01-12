6967f10530999842c55ab007a43a664be7da947d
RazUzan <raz1669@gmail.com>
2021-01-12 20:31:41 +0200

    ABU HAzera

--- RazVeDan (1).py
+++ RazVeDan (1).py
@@ -1,10 +1,15 @@
+
+
+
+
+
 import cv2
 import imutils
 import numpy as np
 import time
 
 import serial
-ABU = "asdsad"
+
 frameWidth = 640
 frameHeight = 480
 cap = cv2.VideoCapture(0)
@@ -15,23 +20,27 @@ cap.set(10,150)
 GREEN = 0
 RED = 1
 BLUE = 2
-K = 2
+DAN = 3
+K = 3
 
 FORWARD = 0
 RIGHT = 1
 LEFT = 2
 
-OBJECTS_TYPE = {RED: ("Target", (0, 0, 255)), GREEN: ("Obsticle", (0, 255, 0)), BLUE: ("Robot", (255, 0, 0))}
+OBJECTS_TYPE = {RED: ("Target", (0, 0, 255)), GREEN: ("Obsticle", (0, 255, 0)), BLUE: ("Robot", (255, 0, 0)), DAN: ("Dan", (0, 0, 0))}
 GOING_TO_TARGET = 0
+
 PASSING_BY_OBSTACLE = 1
 
 PORT = 'COM9'
-BLUETOOTH = serial.Serial(PORT, 9600)
-BLUETOOTH.flushInput()
+#BLUETOOTH = serial.Serial(PORT, 9600)
+#BLUETOOTH.flushInput()
 
-myColors = [[50,72,111,82,145,172],  #green
-            [171,101,174,179,252,255],#red
-            [66,48,153,110,255,255]]  #blue
+myColors = [[0, 100, 0, 113, 199, 170],    #green
+            [171,101,174,179,252,255], #red
+            [66,48,153,110,255,255]]#   #blue
+            # list((12, 91, 83, 60, 255, 251))]
+
 
 def create_color_trackbar(name="tracker"):
     def nothing(x):
@@ -70,7 +79,7 @@ def findColor(img,myColors):
         if p:
             newPoints += p
             cnts += c
-    if newPoints:
+    if len(newPoints):
         newPoints, indices = remove_noise_contours(newPoints)
         cnts = np.array(cnts)[indices]
     return np.array(newPoints).astype(int), cnts
@@ -83,7 +92,7 @@ def get_contours(img, color):
     points, res_contours = [], []
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        if area > 100:
+        if area > 170:
             ((mid_x, mid_y), r) = cv2.minEnclosingCircle(cnt)
             mid_x, mid_y = int(mid_x), int(mid_y)
             if mid_x != 0 and mid_y != 0:
@@ -130,90 +139,141 @@ def remove_noise_contours(points):
         dominant_p = max(curr_points, key=lambda x: x[2])
         res.append(dominant_p)
         indices.append(np.array(np.where(np.all(points==dominant_p, axis=1))).flatten()[0])
-
     return np.array(res), np.array(indices)
 
 
 def draw_cnt(p, cnt):
+
     p = tuple(p)
     text, heu = OBJECTS_TYPE[p[3]]
     x, y, h, w = cv2.boundingRect(cnt)
     cv2.rectangle(imgResult, (x, y), (x + h, y + w), heu, 2)
-    cv2.putText(imgResult, text,
-                (x, y - 5), cv2.FONT_HERSHEY_COMPLEX, 0.7,
-                heu, 2)
+    cv2.putText(imgResult, text, (x, y - 5), cv2.FONT_HERSHEY_COMPLEX, 0.7, heu, 2)
     cv2.drawContours(imgResult, cnt, -1, (90, 26, 170), 3)
     cv2.circle(imgResult, p[:2], p[2] + 10, (60, 200, 110), 2)
 
 
+def get_new_colors_range(img):
+    if cv2.getTrackbarPos('1_high', "tracker") == -1:
+        create_color_trackbar("tracker")
+    x = get_color_trackbar_values()
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower = np.array(x[0:3])
+    upper = np.array(x[3:6])
+    mask = cv2.inRange(imgHSV, lower, upper)
+    kernel = np.ones((5,5), dtype="uint8")
+    mask = cv2.erode(mask, kernel, iterations=1)
+    mask = cv2.dilate(mask, kernel, iterations=1)
+    cv2.imshow("tracker", mask)
+    print(x)
+
+
 if __name__ == '__main__':
+
+    # Get new colors range
+    # while True:
+    #     success, img = cap.read()
+    #     imgResult = img.copy()
+    #     get_new_colors_range(img)
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    #         exit(0)
 
     iter = 0
     curr_state = GOING_TO_TARGET
+    g_obstacles = None
+    g_targets = None
+    obstacles, target = [], []
     while True:
-
         success, img = cap.read()
         imgResult = img.copy()
         newPoints, cnts = findColor(img, myColors)
 
         # Organize points according to role (Robot, obsticles, target)
         indices = np.where(newPoints == BLUE)[0]
-        robot = newPoints[indices]
-        robot_cnt = cnts[indices]
+        if len(indices):
+            robot = newPoints[indices]
+            robot_cnt = cnts[indices]
+            [draw_cnt(p, c) for p, c in zip(robot, robot_cnt)]
 
         indices = np.where(newPoints == RED)[0]
-        target = newPoints[indices]
-        target_cnt = newPoints[indices]
-
-        if not robot or not target:
-            continue
+        if len(indices):
+            target = newPoints[indices]
+            target_cnt = cnts[indices]
+            [draw_cnt(p, c) for p, c in zip(target, target_cnt)]
 
         indices = np.where(newPoints == GREEN)[0]
-        obstacles = newPoints[indices]
-        obstacles_cnts = newPoints[indices]
+        if len(indices):
+            obstacles = newPoints[indices]
+            obstacles_cnts = cnts[indices]
+            [draw_cnt(p, c) for p, c in zip(obstacles, obstacles_cnts)]
 
-        # Move car
-        angle_to_target = find_angel(robot, target)
-        angles_to_obstacles = [find_angel(robot, x) for x in obstacles]
+        indices = np.where(newPoints == DAN)[0]
+        if len(indices):
+            dan = newPoints[indices]
+            dan_cnt = cnts[indices]
+            [draw_cnt(p, c) for p, c in zip(dan, dan_cnt)]
 
-        if curr_state == GOING_TO_TARGET:
-            BLUETOOTH.write(str.encode(str(FORWARD)))
-
-            for obst in obstacles:
-                if is_too_close_to_object(robot, obst):
-                    curr_state = PASSING_BY_OBSTACLE
-                    break
-        # curr_state = PASSING_BY_OBSTACLE
-        else:
-            
-            BLUETOOTH.write(str.encode(str(RIGHT)))
-
-
-
-
-        # if len(newPoints) > K - 1:
-        #
-        #     ob1 = tuple(newPoints[0])
-        #     ob2 = tuple(newPoints[-1])
-        #
-        #     print("Current Frame:\n----------------------")
-        #     # print(find_angel(*newPoints[:2]))
-        #     print(f"points {ob1}, {ob2}")
-        #     print(find_distance(ob1, ob2))
-        #     print("r's", ob1[2], ob2[2])
-        #     print("\n\n")
-        #     r1, r2= int(ob1[2]), int(ob2[2])
-        #     for i in [0, -1]:
-        #         draw_cnt(newPoints[i], cnts[i])
-        #     cv2.line(imgResult, ob1[:2], ob2[:2], (0, 0, 0), 2)
-        #     cv2.imshow("Result", imgResult)
-        #     # if cv2.waitKey(0) & 0xFF == ord('q'):
-        #     #     continue
-
-        cv2.imshow("Result",imgResult)
+        cv2.imshow("Result", imgResult)
         time.sleep(0.1)
 
-
         if cv2.waitKey(1) & 0xFF == ord('q'):
+            g_obstacles = obstacles
+            g_targets = target
             break
-        iter += 1
+
+    # while True:
+    #
+    #     success, img = cap.read()
+    #     imgResult = img.copy()
+    #     newPoints, cnts = findColor(img, myColors)
+    #
+    #     # Organize points according to role (Robot, obsticles, target)
+    #     indices = np.where(newPoints == BLUE)[0]
+    #     robot = newPoints[indices]
+    #     robot_cnt = cnts[indices]
+    #
+    #     indices = np.where(newPoints == RED)[0]
+    #     target = newPoints[indices]
+    #     target_cnt = newPoints[indices]
+    #
+    #     if not robot or not target:
+    #         continue
+    #
+    #     indices = np.where(newPoints == GREEN)[0]
+    #     obstacles = newPoints[indices]
+    #     obstacles_cnts = newPoints[indices]
+    #
+    #     cv2.imshow("Result", imgResult)
+    #     time.sleep(0.1)
+    #
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    #         g_obstacles = obstacles
+    #         g_targets = target
+    #         break
+
+# iter += 1
+
+
+# if not robot or not target:
+#     continue
+#
+# indices = np.where(newPoints == GREEN)[0]
+# obstacles = newPoints[indices]
+# obstacles_cnts = newPoints[indices]
+#
+# # Move car
+# angle_to_target = find_angel(robot, target)
+# angles_to_obstacles = [find_angel(robot, x) for x in obstacles]
+#
+# if curr_state == GOING_TO_TARGET:
+#     BLUETOOTH.write(str.encode(str(FORWARD)))
+#
+#     for obst in obstacles:
+#         if is_too_close_to_object(robot, obst):
+#             curr_state = PASSING_BY_OBSTACLE
+#             break
+# # curr_state = PASSING_BY_OBSTACLE
+# else:
+#
+#     BLUETOOTH.write(str.encode(str(RIGHT)))
+
